f3328bcb6ccac0606401e424ccaf9a505d61bb2f
ajay <ajaybeniwal070@gmail.com>
2023-01-05 13:35:25 +0530

    new end point add

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import io
 import os
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI ,File, UploadFile,HTTPException
+
 
 from PIL import Image
 from io import BytesIO
@@ -29,6 +30,52 @@ async def obj_detect(image_url:str):
     response = requests.get(image_url)
     image_bytes = io.BytesIO(response.content)    
     original_image = Image.open(image_bytes)  #.convert("RGBA")
+
+    original_image.save("input.jpg")
+    img_path = "input.jpg"
+
+    # detector = ObjectDetection()
+    # detector.setModelTypeAsRetinaNet()
+    # detector.setModelPath("resnet50.h5")
+    # detector.loadModel()
+
+    detection = detector.detectObjectsFromImage(input_image=img_path, output_image_path="result.jpg")
+    # im = Image.open("result.jpg")
+    # im.show()
+
+    data = detection
+    LIST = data
+    shorted = ( ", ".join( repr(e) for e in LIST ))
+    dic2 = eval(shorted)
+
+    s_no = 1
+    bb = [0]
+    name = []
+
+    if len(data) > 1 :
+
+        for i in range (len(dic2)):
+            Dict = dict(dic2[i])
+            # print((Dict['name']))
+            name.append((Dict['name']))
+            # bb.append(Dict['box_points'])
+            # s_no+=1
+
+    else:
+        Dict = dict(dic2)
+        name.append((Dict['name']))
+
+    print(name)
+    return (name)
+
+
+
+
+@app.post("/object_detection_file")
+async def object_detection_by_file(insert_image: UploadFile=File(...),):
+    
+    contents = await insert_image.read()    
+    original_image = Image.open(BytesIO(contents))  #.convert("RGBA")
 
     original_image.save("input.jpg")
     img_path = "input.jpg"
